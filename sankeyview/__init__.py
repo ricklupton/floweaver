d8b9ece7154abc27ee142c7c326f3092a73607fc
Rick Lupton <rcl33@cam.ac.uk>
2016-05-26 15:59:09 +0100

    add extra import to __init__.py

--- sankeyview/__init__.py
+++ sankeyview/__init__.py
@@ -6,3 +6,4 @@ from .grouping import Grouping, Group
 from .view_definition import ViewDefinition
 from .sankey_view import sankey_view
 from .hierarchy import Hierarchy
+from .graph_to_sankey import graph_to_sankey
