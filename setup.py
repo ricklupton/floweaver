86e9b30297b743519be52451a23ca96549b49780
dnuka <csx@tuta.io>
2018-05-10 13:49:13 +0530

    removed python2 compatibility claim

--- setup.py
+++ setup.py
@@ -39,8 +39,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
