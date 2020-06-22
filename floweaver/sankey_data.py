6dc83b2c84a7a0b9584f67f8be40ff559df9ff89
Rick Lupton <mail@ricklupton.name>
2020-06-22 14:45:52 +0100

    Formatting changes

--- floweaver/sankey_data.py
+++ floweaver/sankey_data.py
@@ -31,119 +31,132 @@ class SankeyData(object):
 
     def to_json(self, filename=None, format=None):
         """Convert data to JSON-ready dictionary."""
-        if format == 'widget':
+        if format == "widget":
             data = {
-                'nodes': [n.to_json(format) for n in self.nodes],
-                'links': [l.to_json(format) for l in self.links],
-                'order': self.ordering.layers,
-                'groups': self.groups,
+                "nodes": [n.to_json(format) for n in self.nodes],
+                "links": [l.to_json(format) for l in self.links],
+                "order": self.ordering.layers,
+                "groups": self.groups,
             }
         else:
             data = {
-                'format': 'sankey-v2',
-                'metadata': {
-                    'title': 'A Sankey diagram',
-                    'authors': [],
-                    'layers': self.ordering.layers,
+                "format": "sankey-v2",
+                "metadata": {
+                    "title": "A Sankey diagram",
+                    "authors": [],
+                    "layers": self.ordering.layers,
                 },
-                'nodes': [n.to_json(format) for n in self.nodes],
-                'links': [l.to_json(format) for l in self.links],
-                'groups': self.groups,
+                "nodes": [n.to_json(format) for n in self.nodes],
+                "links": [l.to_json(format) for l in self.links],
+                "groups": self.groups,
             }
 
         if filename is None:
             return data
         else:
-            with open(filename, 'wt') as f:
+            with open(filename, "wt") as f:
                 json.dump(data, f)
 
-    def to_widget(self, width=700, height=500, margins=None,
-                  align_link_types=False, link_label_format='', 
-                  link_label_min_width=5, debugging=False):
+    def to_widget(
+        self,
+        width=700,
+        height=500,
+        margins=None,
+        align_link_types=False,
+        link_label_format="",
+        link_label_min_width=5,
+        debugging=False,
+    ):
 
         if SankeyWidget is None:
-            raise RuntimeError('ipysankeywidget is required')
+            raise RuntimeError("ipysankeywidget is required")
 
         if margins is None:
             margins = {
-                'top': 25,
-                'bottom': 10,
-                'left': 130,
-                'right': 130,
+                "top": 25,
+                "bottom": 10,
+                "left": 130,
+                "right": 130,
             }
 
-        value = self.to_json(format='widget')
-        widget = SankeyWidget(nodes=value['nodes'],
-                              links=value['links'],
-                              order=value['order'],
-                              groups=value['groups'],
-                              align_link_types=align_link_types,
-                              linkLabelFormat=link_label_format,
-                              linkLabelMinWidth=link_label_min_width,
-                              layout=Layout(width=str(width), height=str(height)),
-                              margins=margins)
+        value = self.to_json(format="widget")
+        widget = SankeyWidget(
+            nodes=value["nodes"],
+            links=value["links"],
+            order=value["order"],
+            groups=value["groups"],
+            align_link_types=align_link_types,
+            linkLabelFormat=link_label_format,
+            linkLabelMinWidth=link_label_min_width,
+            layout=Layout(width=str(width), height=str(height)),
+            margins=margins,
+        )
 
         if debugging:
             output = Output()
+
             def callback(_, d):
                 with output:
                     clear_output()
                 if not d:
                     return
-                link = [l for l in self.links
-                        if l.source == d['source']
-                        and l.target == d['target']
-                        and l.type == d['type']]
+                link = [
+                    l
+                    for l in self.links
+                    if l.source == d["source"]
+                    and l.target == d["target"]
+                    and l.type == d["type"]
+                ]
                 assert len(link) == 1
                 link = link[0]
                 with output:
-                    display('Flows in dataset contributing to this link:')
+                    display("Flows in dataset contributing to this link:")
                     if self.dataset:
                         display(self.dataset._table.loc[link.original_flows])
                     else:
                         display(link.original_flows)
+
             widget.on_link_clicked(callback)
             return VBox([widget, output])
         else:
             return widget
 
 
-
 @attr.s(slots=True, frozen=True)
 class SankeyNode(object):
     id = attr.ib(validator=attr.validators.instance_of(str))
     title = attr.ib(default=None, validator=_validate_opt_str)
-    direction = attr.ib(validator=_validate_direction, default='R')
+    direction = attr.ib(validator=_validate_direction, default="R")
     hidden = attr.ib(default=False)
     style = attr.ib(default=None, validator=_validate_opt_str)
 
     def to_json(self, format=None):
         """Convert node to JSON-ready dictionary."""
-        if format == 'widget':
+        if format == "widget":
             return {
-                'id': self.id,
-                'title': self.title if self.title is not None else self.id,
-                'direction': self.direction.lower(),
-                'hidden': self.hidden is True or self.title == '',
-                'type': self.style if self.style is not None else 'default',
+                "id": self.id,
+                "title": self.title if self.title is not None else self.id,
+                "direction": self.direction.lower(),
+                "hidden": self.hidden is True or self.title == "",
+                "type": self.style if self.style is not None else "default",
             }
         else:
             return {
-                'id': self.id,
-                'title': self.title if self.title is not None else self.id,
-                'style': {
-                    'direction': self.direction.lower(),
-                    'hidden': self.hidden is True or self.title == '',
-                    'type': self.style if self.style is not None else 'default',
+                "id": self.id,
+                "title": self.title if self.title is not None else self.id,
+                "style": {
+                    "direction": self.direction.lower(),
+                    "hidden": self.hidden is True or self.title == "",
+                    "type": self.style if self.style is not None else "default",
                 },
             }
 
 
 def _validate_opacity(instance, attr, value):
     if not isinstance(value, float):
-        raise ValueError('opacity must be a number')
+        raise ValueError("opacity must be a number")
     if value < 0 or value > 1:
-        raise ValueError('opacity must be between 0 and 1')
+        raise ValueError("opacity must be between 0 and 1")
 
 
 @attr.s(slots=True, frozen=True)
@@ -160,29 +173,24 @@ class SankeyLink(object):
 
     def to_json(self, format=None):
         """Convert link to JSON-ready dictionary."""
-        if format == 'widget':
+        if format == "widget":
             return {
-                'source': self.source,
-                'target': self.target,
-                'type': self.type,
-                'time': self.time,
-                'value': self.value,
-                'title': self.title,
-                'color': self.color,
-                'opacity': self.opacity,
+                "source": self.source,
+                "target": self.target,
+                "type": self.type,
+                "time": self.time,
+                "value": self.value,
+                "title": self.title,
+                "color": self.color,
+                "opacity": self.opacity,
             }
         else:
             return {
-                'source': self.source,
-                'target': self.target,
-                'type': self.type,
-                'title': self.title,
-                'time': self.time,
-                'data': {
-                    'value': self.value,
-                },
-                'style': {
-                    'color': self.color,
-                    'opacity': self.opacity,
-                }
+                "source": self.source,
+                "target": self.target,
+                "type": self.type,
+                "title": self.title,
+                "time": self.time,
+                "data": {"value": self.value,},
+                "style": {"color": self.color, "opacity": self.opacity,},
             }
--- floweaver/weave.py
+++ floweaver/weave.py
@@ -14,16 +14,18 @@ from palettable.colorbrewer import qualitative, sequential
 
 # From matplotlib.colours
 def rgb2hex(rgb):
-    'Given an rgb or rgba sequence of 0-1 floats, return the hex string'
-    return '#%02x%02x%02x' % tuple([int(np.round(val * 255)) for val in rgb[:3]])
+    "Given an rgb or rgba sequence of 0-1 floats, return the hex string"
+    return "#%02x%02x%02x" % tuple([int(np.round(val * 255)) for val in rgb[:3]])
 
 
-def weave(sankey_definition,
-          dataset,
-          measures='value',
-          link_width=None,
-          link_color=None,
-          palette=None):
+def weave(
+    sankey_definition,
+    dataset,
+    measures="value",
+    link_width=None,
+    link_color=None,
+    palette=None,
+):
 
     # Accept DataFrames as datasets -- assume it's the flow table
     if isinstance(dataset, pd.DataFrame):
@@ -42,20 +44,27 @@ def weave(sankey_definition,
 
     # Get the flows selected by the bundles
     bundle_flows, unused_flows = dataset.apply_view(
-        sankey_definition.nodes, bundles2, sankey_definition.flow_selection)
+        sankey_definition.nodes, bundles2, sankey_definition.flow_selection
+    )
 
     # Calculate the results graph (actual Sankey data)
-    GR, groups = results_graph(GV2,
-                               bundle_flows,
-                               flow_partition=sankey_definition.flow_partition,
-                               time_partition=sankey_definition.time_partition,
-                               measures=measures)
+    GR, groups = results_graph(
+        GV2,
+        bundle_flows,
+        flow_partition=sankey_definition.flow_partition,
+        time_partition=sankey_definition.time_partition,
+        measures=measures,
+    )
 
     # Default link width is same as default measure
     if link_width is None:
         if not isinstance(measures, str):
-            raise ValueError(('If you set a complicated measure function, '
-                              'you need to set link_width too.'))
+            raise ValueError(
+                (
+                    "If you set a complicated measure function, "
+                    "you need to set link_width too."
+                )
+            )
         link_width = measures
 
     if callable(link_width):
@@ -63,35 +72,35 @@ def weave(sankey_definition,
     elif isinstance(link_width, str):
         get_value = lambda link, measures: float(measures[link_width])
     else:
-        raise ValueError('link_width must be a str or callable')
+        raise ValueError("link_width must be a str or callable")
 
     # Default link color is categorical scale based on link type
     if link_color is None:
-        link_color = CategoricalScale('type', palette=palette)
+        link_color = CategoricalScale("type", palette=palette)
     elif isinstance(link_color, str):
         link_color = CategoricalScale(link_color, palette=palette)
     elif not callable(link_color):
-        raise TypeError('link_color must be a str or callable')
+        raise TypeError("link_color must be a str or callable")
 
     # Set domain for quantitative colors, if not already set
-    if hasattr(link_color, 'set_domain_from'):
-        link_color.set_domain_from([data['measures'] for _, _, data in GR.edges(data=True)])
+    if hasattr(link_color, "set_domain_from"):
+        link_color.set_domain_from(
+            [data["measures"] for _, _, data in GR.edges(data=True)]
+        )
 
     # Package result
     links = [
         make_link(get_value, link_color, v, w, m, t, data)
         for v, w, (m, t), data in GR.edges(keys=True, data=True)
     ]
-    nodes = [
-        make_node(u, data)
-        for u, data in GR.nodes(data=True)
-    ]
+    nodes = [make_node(u, data) for u, data in GR.nodes(data=True)]
     result = SankeyData(nodes, links, groups, GR.ordering.layers, dataset)
 
     return result
 
 
 # maybe this function should be customisable?
+
 
 def make_link(get_value, get_color, v, w, m, t, data):
     link = SankeyLink(
@@ -112,9 +121,9 @@ def make_link(get_value, get_color, v, w, m, t, data):
 def make_node(u, data):
     return SankeyNode(
         id=u,
-        title=data.get('title'),
-        style=data.get('type'),
-        direction=data.get('direction', 'R'),
+        title=data.get("title"),
+        style=data.get("type"),
+        direction=data.get("direction", "R"),
         # XXX not setting hidden here -- should have logic here or in to_json()?
     )
 
@@ -122,15 +131,16 @@ def make_node(u, data):
 def prep_qualitative_palette(G, palette):
     # qualitative colours based on material
     if palette is None:
-        palette = 'Pastel1_8'
+        palette = "Pastel1_8"
 
     if isinstance(palette, str):
         try:
             palette = getattr(qualitative, palette).hex_colors
         except AttributeError:
-            raise ValueError('No qualitative palette called {}'.format(palette)) from None
+            raise ValueError(
+                "No qualitative palette called {}".format(palette)
+            ) from None
 
     if not isinstance(palette, dict):
         materials = sorted(set([m for v, w, (m, t) in G.edges(keys=True)]))
-        palette = {m: v
-                    for m, v in zip(materials, itertools.cycle(palette))}
+        palette = {m: v for m, v in zip(materials, itertools.cycle(palette))}
